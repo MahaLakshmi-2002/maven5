dc1c87607222fd00d85d128800d04e91118430a6
MahaLakshmi <mahavijay14@gmail.com>
2023-02-14 16:32:32 +0530

    commit

---
+++ src/main/java/calc/Calc.java
@@ -0,0 +1,17 @@
+package calc;
+
+public class Calc {
+	int a,b;
+	public int addition(int a, int b) {
+		return a+b;
+	}
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+	public int multiplication(int a, int b) {
+		return a*b;
+	}
+	public int division(int a, int b) {
+		return a/b;
+	}
+}
---
+++ src/test/java/calctest/Calctest.java
@@ -0,0 +1,56 @@
+package calctest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import calc.Calc;
+
+public class Calctest {
+	
+	public static Calc objCalculator;
+
+	@Before
+	public void setup() throws Exception {
+		
+		objCalculator=new Calc();
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+		System.out.println("closing ");
+	}
+
+	@Test
+	public void additionTest() {
+		
+		assertEquals(25, objCalculator.addition(12, 13));
+		
+	}
+	
+	@Test
+	public void substractTest() {
+		assertEquals(-1,objCalculator.subtract(12, 13));
+		
+	}
+	
+	@Test
+	public void divisionTest() {
+		assertEquals(0,objCalculator.division(12, 13));
+		
+	}
+	
+	
+	@Test
+	public void MutlitplicationTest() {
+		assertEquals(156,objCalculator.multiplication(12, 13));
+		
+	}
+	
+	
+
+}
